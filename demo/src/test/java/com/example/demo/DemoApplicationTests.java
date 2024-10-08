26d1b84ab54631ea0b0094faf01fd8af2ef445a9
bojo992 <93447305+zed992@users.noreply.github.com>
2024-10-08 12:29:43 +0100

    test21

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Object test = new Object();
+        assert test != null;
     }
 
 }
